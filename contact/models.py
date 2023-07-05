48a8b3044c938a8a0255f3426d16ef23d25ead0e
Laisa Nascimento <llaisanasc@gmail.com>
2023-07-05 16:42:30 -0300

    ImageField, media_url e media_root

--- contact/models.py
+++ contact/models.py
@@ -17,6 +17,10 @@ class Contact(models.Model):
     #campo opcional
     description = models.TextField(blank=True)
 
+    show = models.BooleanField(default=True)
+    picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
+
+
     #retorna como nome de objeto
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'
