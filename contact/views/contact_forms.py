a9021fbc4ad4dc202b85acc5b1cddcd047f8ae5d
Laisa Nascimento <llaisanasc@gmail.com>
2023-07-10 13:24:59 -0300

    formulario dinamico com django para create contact

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,14 +1,26 @@
 from django.shortcuts import  get_object_or_404, render, redirect
-from contact.models import Contact
 from django.http import Http404
 from django.db.models import Q
 from django.core.paginator import Paginator
+from contact.models import Contact
+from contact.forms import ContactForm
 
 
 def create(request):
- 
+    if request.method == 'POST':
+
+        context = {
+            'form': ContactForm(request.POST)
+        }
+
+        return render(
+            request, 
+            'contact/create.html',
+            context
+        )
+    
     context = {
-       
+        'form': ContactForm()
     }
 
     return render(
