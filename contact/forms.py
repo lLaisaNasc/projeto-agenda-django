48c22ec22d5d61d555ca9e4bf41d064111cb20f4
Laisa Nascimento <llaisanasc@gmail.com>
2023-07-11 16:11:12 -0300

    UserCreationForm para criar novos usuarios

--- contact/forms.py
+++ contact/forms.py
@@ -2,6 +2,7 @@
 from django import forms
 from django.core.exceptions import ValidationError
 from . import models
+from django.contrib.auth.forms import UserCreationForm
 
 class ContactForm(forms.ModelForm):
 
@@ -39,16 +40,19 @@ class ContactForm(forms.ModelForm):
         
         return super().clean()
     
-def clean_first_name(self):
-    first_name = self.cleaned_data.get('first_name')
+    def clean_first_name(self):
+        first_name = self.cleaned_data.get('first_name')
 
-    if first_name == 'ABC':
-            self.add_error(
-                'first_name',
-                ValidationError(
-                    'Veio do add_error',
-                    code='invalid'
+        if first_name == 'ABC':
+                self.add_error(
+                    'first_name',
+                    ValidationError(
+                        'Veio do add_error',
+                        code='invalid'
+                    )
                 )
-            )
 
-    return first_name
+        return first_name
+    
+class RegisterForm(UserCreationForm):
+     ...
