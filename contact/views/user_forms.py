5dcfc50089390d25610b9f72fe06eac2042d6f17
Laisa Nascimento <llaisanasc@gmail.com>
2023-07-12 11:06:08 -0300

    form, view e url para update user

--- contact/views/user_forms.py
+++ contact/views/user_forms.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from contact.forms import RegisterForm
+from contact.forms import RegisterForm, RegisterUpdateForm
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib import auth
 
@@ -44,6 +44,34 @@ def login_view(request):
             'form':form
         }
     )
+
+def user_update(request):
+     form = RegisterUpdateForm(instance=request.user)
+
+     if request.method != 'POST':
+        return render(
+            request, 
+            'contact/user_update.html',
+            {
+                'form':form
+            }
+        )
+    
+     form = RegisterUpdateForm(data=request.POST, instance=request.user)
+
+     if not form.is_valid():
+         return render(
+            request, 
+            'contact/user_update.html',
+            {
+                'form':form
+            }
+        )
+     
+     form.save()
+
+     return redirect('contact:user_update')
+         
 
 def logout_view(request):
     auth.logout(request)
